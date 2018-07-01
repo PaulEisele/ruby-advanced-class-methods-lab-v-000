1d538f372a7cd9519c4f857251326dafecb4a504
paulheisele@gmail.com <paulheisele@gmail.com>
2018-07-01 00:18:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,9 @@ class Song
     self.all.sort_by! {|song| song.name }
   end
 
+  def self.find_by_name
+    self.all.find{|song| song.name == name}
+  end
 
   def self.new_by_name(name)
     song = self.new
@@ -42,8 +45,8 @@ class Song
     song
   end
 
-  def SONG.CREATE_FROM_FILENAME
-
+  def self.find_or_create_by_name
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def SONG.DESTROY_ALL
